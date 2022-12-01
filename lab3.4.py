cb7e4561b7f01356c33f08c71612249dcee4a7b6
Chainsuck <113112022+Chainsuck@users.noreply.github.com>
2022-12-01 11:59:53 +0300

    Add files via upload

---
+++ lab3.4.py
@@ -0,0 +1,10 @@
+n = int(input('Введите любое натуральное число: '))
+print('Составлена ваша Пирамида: ')
+for i in range(1,n+1):
+    for a in range(1, n+1-i):
+        print('', end=' ')
+    for b in range (1,i+1):
+        print(b, end= '')
+    for c in range(i-1,0,-1):
+        print(c, end = '')
+    print()
