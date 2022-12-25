5d44a76b05e050d354c25b31ae33016a16d0b679
Chainsuck <113112022+Chainsuck@users.noreply.github.com>
2022-12-25 16:01:26 +0300

    Update Task5.py

--- lab4/lab4 Tasks/Task5.py
+++ lab4/lab4 Tasks/Task5.py
@@ -1,19 +1,4 @@
-from figures import *
+from figures import circle_area, circle_perimeter
 
 circle_area()
-circle_area(7)
-
 circle_perimeter()
-circle_perimeter(9)
-
-triangle_perimeter()
-triangle_perimeter(4,3,7)
-
-triangle_area()
-triangle_area(1,9,6)
-
-square_perimeter()
-square_perimeter(2)
-
-square_area()
-square_area(7)
